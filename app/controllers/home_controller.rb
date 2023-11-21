d3dd636a1039a28b7e6270d595a74f9ca7018def
Nikita <churilov2017@mail.ru>
2023-11-21 09:12:35 +0300

    initial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,19 +12,10 @@ class HomeController < ApplicationController
           return
         end
       end
-      sum_negative_elements = arr_v.select { |x| x < 0 }.sum
-
-      first_even_index = arr_v.find_index { |x| x.even? }
-      max_index = arr_v.index(arr_v.max)
-
-      if first_even_index
-        arr_v[first_even_index] = sum_negative_elements
-      else
-        arr_v[max_index] = sum_negative_elements
-      end
+      
 
 
-      @result = arr_v.join(' ')
+      @result = task_26(arr_v).join(' ')
     else
       @result = []
     end
@@ -38,5 +29,48 @@ class HomeController < ApplicationController
       el.to_i
     end
   end
+
+
+def task_26(arr)
+  max_elem = arr.max
+  min_index = arr.find_index(arr.min)
+  if arr.select { |x| x < 0 }.count!=0
+    arr[arr.find_index { |x| x<0 }] = max_elem
+  else
+    arr[min_index] = max_elem
+  end
+  return arr
 end
+
+
+
+def task_2(arr)
+  max_sum = 0
+  start_index = 0
+  first_even1_index = arr.find_index { |x| x.even? }
+  end_index = arr.length - 1
+
+  while start_index < end_index
+    current_sum = arr[start_index] + arr[end_index]
+    if current_sum > max_sum
+      max_sum = current_sum 
+      @st_id = start_index 
+      @ed_id = end_index
+    end
+
+    start_index += 1
+    end_index -= 1
+  end
+  arr[first_even1_index] = max_sum
+  return arr 
+end
+
+def task_30(arr)
+end
+end
+
+
+
+
+
 
